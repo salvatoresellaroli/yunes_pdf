71e496e0b1a8b56b5c9e376fe7f927188d27d618
Salvatore Sellaroli <salvatore@tekmind.it>
2023-10-24 14:15:35 +0200

    modifiche pdf energia

--- Controllers/energia/preventivo.php
+++ Controllers/energia/preventivo.php
@@ -3,45 +3,56 @@ include '../../Core/init.php';
 require_once __DIR__ . '/vendor/autoload.php';
 try {
 
-    $mpdf = new \Mpdf\Mpdf( [
+    $mpdf = new \Mpdf\Mpdf([
         'default_font' => 'Calibri'
-    ] );
+    ]);
 
-    if ( $_SERVER[ 'REQUEST_METHOD' ] === 'OPTIONS' ) {
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
         return 0;
     }
 
     # Dati
-    $dati = json_decode( file_get_contents( 'php://input' ), true );
-    
+
+    $log->scrivi("PDF_energia", logType::start, "[Inzio] ");
+
+    $dati = json_decode(file_get_contents('php://input'), true);
+
     $obj = $dati['DocumentElement']['value'];
 
-    $nome_referente = 'NOME REFERENTE';
-    $data_preventivo = 'OTT 04,2023';
-    $nome_cliente = 'NOME CLIENTE';
+    if (empty($dati)) {
+        $log->scrivi("PDF_energia", logType::middle, "[Errore] json vuoto ");
+        throw new Exception("Dati json vuoto.");
+    }
+
+
+    $log->scrivi("PDF_energia", logType::middle, "[json] :", json_encode($obj, JSON_PRETTY_PRINT));
+
+    $nome_referente = $obj['consulente'];
+    $data_preventivo = dataAttualePDF();
+    $nome_cliente = $obj['anagrafica'];
     $indirizzo_cliente = $obj['indirizzo'];
     $potenzaP = $obj['dimensioneFV_kWp_ibrio'];
-    $batteria_kw = $obj['batteria']. ' kW';
-    $txt_DescrizionePannello = 'F.p.o. di moduli fotovoltaici monocristallino di potenza 430Wp ognuno. ( scheda tecnica allegata )';
-    $txt_MarcaPannello = 'LONGI modello Hi-Mo6 LR5-54HTH-430M (monocristallino)';
-    $numeroPannelli = '85';
-    $txt_DescrizioneInverter = 'F.p.o. di inverter,6kW completo di garanzia decennale (scheda tecnica allegata)';
-    $txt_MarcaInverter = 'SOLAX modello X1-HYBRID-G4-6KTL'; 
-    $numeroInverter = '2';
-    $txt_DescrizioneBatteria = 'F.p.o. di batteria d’accumulo per un totale di 11,6kw.';
-    $txt_MarcaBatteria = 'SOLAX modello T—BAT-SYS-H58 da 5.8kW';
-    $numeroBatterie = '8';
-    $totalePreventivo = '€ 25.000';
-    $totaleDetrazioniFiscale = '€ 12.500';
-    $firmaConsulente = 'Firma Consulente';
-    $firmaCliente = 'Firma Cliente';
+    $batteria_kw =   ($obj['ibrido'] == 'true') ? $obj['batteria'] . ' kW' : '';
+    $txt_DescrizionePannello = $obj['nome_pannelli'];
+    $txt_MarcaPannello = $obj['brand_pannelli'];
+    $numeroPannelli = ($obj['ibrido'] == 'true') ? $obj['numero_pannelli_ibrido'] : $obj['numero_pannelli_senza_batteria'];
+    $txt_DescrizioneInverter = $obj['nome_inverter'];
+    $txt_MarcaInverter = $obj['brand_inverter'];
+    $numeroInverter = $obj['prod_inverter_quantity'];
+    $txt_DescrizioneBatteria =  ($obj['ibrido'] == 'true') ? $obj['nome_batterie'] : '';
+    $txt_MarcaBatteria = ($obj['ibrido'] == 'true') ? $obj['brand_batterie'] : '';
+    $numeroBatterie = ($obj['ibrido'] == 'true') ? $obj['prod_batterie_quantity'] : '';
+    $totalePreventivo =   ($obj['ibrido'] == 'true') ? $obj['totale_costo_ibrdo'] : $obj['totale_costo_senza_batteria'];
+    $totaleDetrazioniFiscale = intval($totalePreventivo / 2);
+    $firmaConsulente = $obj['consulente'];
+    $firmaCliente = $obj['anagrafica'];
     $attach_map = $obj['attach_map'];
     $attach_set_result_1 = $obj['attach_set_result_1'];
     $attach_set_result_2 = $obj['attach_set_result_2'];
     $attach_set_result_3 = $obj['attach_set_result_3'];
 
-    $mpdf->SetTitle( 'PDA - Yunes CRM' );
-    $mpdf->SetFont( 'Calibri', 'B', 14 );
+    $mpdf->SetTitle('PDA - Yunes CRM');
+    $mpdf->SetFont('Calibri', 'B', 14);
     //* ///////////////////////////////////////////////////////////////////////////
     $mpdf->SetHTMLFooter('<hr>
     <table style="font-size: 9pt;" width="100%">
@@ -60,115 +71,136 @@ try {
     ###################################################################################################
     # PAGINA n.1 - Cover
     $mpdf->AddPage();
-    $mpdf->Image( 'img/cover_page.jpg', 0, 0, 210, 297, 'jpg', '', true, false );
+    $mpdf->Image('img/cover_page.jpg', 0, 0, 210, 297, 'jpg', '', true, false);
 
-    $mpdf->SetFont( 'Calibri', 'B', 18 );
+    $mpdf->SetFont('Calibri', 'B', 18);
 
-    $mpdf->SetTextColor( '#92d14f' );
+    $mpdf->SetTextColor('#92d14f');
     # Nome referente
-    $mpdf->WriteText( 135, 99, strtoupper( $nome_referente  ) );
-    $mpdf->SetTextColor( '#000000' );
+    $mpdf->WriteText(135, 99, strtoupper($nome_referente));
+    $mpdf->SetTextColor('#000000');
     # Data preventivo
-    $mpdf->WriteText( 145, 118, strtoupper( $data_preventivo ) );
+    $mpdf->WriteText(145, 118, strtoupper($data_preventivo));
 
     # Nome cliente
-    $mpdf->SetFont( 'Calibri', 'B', 14 );
-    $mpdf->SetTextColor( '#92d14f' );
-    $mpdf->WriteText( 20, 222, strtoupper( $nome_cliente ) );
+    $mpdf->SetFont('Calibri', 'B', 14);
+    $mpdf->SetTextColor('#92d14f');
+    $mpdf->WriteText(20, 222, strtoupper($nome_cliente));
 
     # Indirizzo di fornitura
-    $mpdf->WriteText( 20, 253, strtoupper( $indirizzo_cliente ) );
-    $mpdf->SetTextColor( '#000000' );
+    $mpdf->SetXY(20, 245);
+    $indirizzoInstallazione = "<p style ='width: 300px;margin-left:20px;font-size:18px; color:#92d14f;font-weight: bold;'>$indirizzo_cliente </p>";
+    $mpdf->WriteHTML($indirizzoInstallazione);
+    $mpdf->SetTextColor('#000000');
 
     ###################################################################################################
     # PAGINA n.2
     $mpdf->AddPage();
-    $mpdf->Image( 'pagine/2.jpg', 0, 0, 210, 297, 'jpg', '', true, false );
+    $mpdf->Image('pagine/2.jpg', 0, 0, 210, 297, 'jpg', '', true, false);
 
     # Potenza P =
-    $mpdf->SetFont( 'Calibri', 'R', 22 );
-    $mpdf->WriteText( 137.8, 66.4, $potenzaP );
+    $mpdf->SetFont('Calibri', 'R', 22);
+    $mpdf->WriteText(137.8, 66.4, $potenzaP);
 
     # SE SCEGLIERE BATTERIA
-    $mpdf->WriteText( 43, 77.4, 'BATTERIA D\'ACCUMULO DI ' . $batteria_kw);
+    $mpdf->WriteText(43, 77.4, 'BATTERIA D\'ACCUMULO DI ' . $batteria_kw);
 
     # MODULI FOTOVOLTAICI
-    $mpdf->SetY(125);
-    $descizionePannello = '<p style ="width: 380px;font-size:18px; margin-left: -25px;font-weight: bold;"> '. $txt_DescrizionePannello .'</p>';
-    $mpdf->WriteHTML($descizionePannello);    
     $mpdf->SetY(130);
-    $marcaPannello = '<p style ="width: 136px;font-size:14px; margin-left: 366px;font-weight: bold;">'.$txt_MarcaPannello.'</p>';
+    $descizionePannello = "<p style ='width: 380px;font-size:18px; margin-left: -25px;font-weight: bold;'>$txt_DescrizionePannello </p>";
+    $mpdf->WriteHTML($descizionePannello);
+    $mpdf->SetY(130);
+    $marcaPannello = "<p style ='width: 136px;font-size:14px; margin-left: 366px;font-weight: bold;'>$txt_MarcaPannello</p>";
     $mpdf->WriteHTML($marcaPannello);
-    $mpdf->SetFont( 'Calibri', 'B', 22 );
-    $mpdf->WriteText( 165, 143, $numeroPannelli);
+    $mpdf->SetFont('Calibri', 'B', 22);
+    $mpdf->WriteText(165, 143, $numeroPannelli);
 
     # INVERTER
     $mpdf->SetY(158);
-    $descizioneInverter = '<p style ="width: 380px;font-size:18px; margin-left: -25px;font-weight: bold;">'.$txt_DescrizioneInverter.'</p>';
-    $mpdf->WriteHTML($descizioneInverter);    
+    $descizioneInverter = "<p style ='width: 380px;font-size:18px; margin-left: -25px;font-weight: bold;'>$txt_DescrizioneInverter</p>";
+    $mpdf->WriteHTML($descizioneInverter);
     $mpdf->SetY(159);
-    $marcaInverter = '<p style ="width: 136px;font-size:14px; margin-left: 366px;font-weight: bold;">'.$txt_MarcaInverter .'</p>';
+    $marcaInverter = "<p style ='width: 136px;font-size:14px; margin-left: 366px;font-weight: bold;'>$txt_MarcaInverter</p>";
     $mpdf->WriteHTML($marcaInverter);
-    $mpdf->SetFont( 'Calibri', 'B', 22 );
-    $mpdf->WriteText( 165, 170, $numeroInverter);
+    $mpdf->SetFont('Calibri', 'B', 22);
+    $mpdf->WriteText(165, 170, $numeroInverter);
 
     # BATTERIA
     $mpdf->SetY(184);
-    $descrizioneBatteria = '<p style ="width: 380px;font-size:18px; margin-left: -25px;font-weight: bold;">'.$txt_DescrizioneBatteria.'</p>';
-    $mpdf->WriteHTML($descrizioneBatteria);    
+    $descrizioneBatteria = "<p style ='width: 380px;font-size:18px; margin-left: -25px;font-weight: bold;'>$txt_DescrizioneBatteria</p>";
+    $mpdf->WriteHTML($descrizioneBatteria);
     $mpdf->SetY(185);
-    $marcaBatteria = '<p style ="width: 136px;font-size:14px; margin-left: 366px;font-weight: bold;">'.$txt_MarcaBatteria.'</p>';
+    $marcaBatteria = "<p style ='width: 136px;font-size:14px; margin-left: 366px;font-weight: bold;'>$txt_MarcaBatteria</p>";
     $mpdf->WriteHTML($marcaBatteria);
-    $mpdf->SetFont( 'Calibri', 'B', 22 );
-    $mpdf->WriteText( 165, 195, $numeroBatterie);
+    $mpdf->SetFont('Calibri', 'B', 22);
+    $mpdf->WriteText(165, 195, $numeroBatterie);
 
     # Totale Preventivo
-    $mpdf->SetFont( 'Calibri', 'B', 14 );
-    $mpdf->WriteText(170, 270, $totalePreventivo);
+    $mpdf->SetFont('Calibri', 'B', 14);
+    $valuta_tot_preventivo = number_format($totalePreventivo, 2, ',', '.');
+    $mpdf->WriteText(165, 270,   "€ " . $valuta_tot_preventivo);
+
+    $mpdf->SetTextColor('#fe0300');
 
     # Totale detrazione fiscale
-    $mpdf->SetTextColor('#fe0300');
-    $mpdf->WriteText(130, 291, $totaleDetrazioniFiscale); 
+    $valuta_totaleDetrazioniFiscale =  number_format($totaleDetrazioniFiscale, 2, ',', '.');
+    $mpdf->WriteText(130, 291, "€ " . $valuta_totaleDetrazioniFiscale);
+
     $mpdf->SetTextColor('#000000');
-    
-    
+
+
     ###################################################################################################
     # PAGINA n.3
     $mpdf->AddPage();
-    $mpdf->Image( 'pagine/3.jpg', 0, 0, 210, 297, 'jpg', '', true, false );
+    $mpdf->Image('pagine/3.jpg', 0, 0, 210, 297, 'jpg', '', true, false);
 
 
     ###################################################################################################
     # PAGINA n.4
     $mpdf->AddPage();
-    $mpdf->Image( 'pagine/4.jpg', 0, 0, 210, 297, 'jpg', '', true, false );
+    $mpdf->Image('pagine/4.jpg', 0, 0, 210, 297, 'jpg', '', true, false);
 
-    $mpdf->SetFont( 'Calibri', 'B', 14 );
-    $mpdf->WriteText(10, 72, $firmaConsulente);
-    $mpdf->WriteText(145, 72, $firmaCliente);
+    $mpdf->SetFont('Calibri', 'B', 14);
+    $mpdf->WriteText(10, 72, strtoupper($firmaConsulente));
+    $mpdf->WriteText(145, 72, strtoupper($firmaCliente));
 
     ###################################################################################################
     # PAGINA n.5
     $mpdf->AddPage('P');
-    $mpdf->Image( 'img/logo_must_energia.jpg', 10, 10, 70, 25, 'jpg', '', true, false );
-    $mpdf->Image($attach_map,30,50);
+    $mpdf->Image('img/logo_must_energia.jpg', 10, 10, 70, 25, 'jpg', '', true, false);
+    $mpdf->Image($attach_map, 30, 50);
+
+    ###################################################################################################
+    # PAGINA n.6   
     $mpdf->AddPage('P');
-    $mpdf->Image( 'img/logo_must_energia.jpg', 10, 10, 70, 25, 'jpg', '', true, false );
-    $mpdf->Image($attach_set_result_1,10,50);
-    $mpdf->Image($attach_set_result_2,10,100);
+    $mpdf->SetTextColor('#92d14f');
+
+    $mpdf->Image('img/logo_must_energia.jpg', 10, 10, 70, 25, 'jpg', '', true, false);
+
+    $mpdf->WriteText(10, 45, "Set Result 2");
+    $mpdf->Image($attach_set_result_2, 10, 50); // Griglia set result 2 (autoconsumi)
+
+    $mpdf->WriteText(10, 90, "Set Result 1");
+    $mpdf->Image($attach_set_result_1, 10, 95); // Griglia set result 1
+
+    ###################################################################################################
+    # PAGINA n.7 
     $mpdf->AddPage('P');
-    $mpdf->Image( 'img/logo_must_energia.jpg', 10, 10, 70, 25, 'jpg', '', true, false );
-    $mpdf->Image($attach_set_result_3,10,60);
+    $mpdf->Image('img/logo_must_energia.jpg', 10, 10, 70, 25, 'jpg', '', true, false);
+    $mpdf->WriteText(10, 55, "Set Result 3");
+    $mpdf->Image($attach_set_result_3, 10, 60); // Griglia set result 3 
+
+
     ###################################################################################################
     # GESTIONE FILE
 
-    $mpdf->SetAuthor( 'Yunes.it' );
+    $mpdf->SetAuthor('Yunes.it');
 
-    $cartellaFile = $obj[ 'pathfile' ];
+    $cartellaFile = $obj['pathfile'];
 
-    if ( !$isDEBUG ) {
+    if (!$isDEBUG) {
         # Verifico se la cartella del paziente esiste altrimenti la creo
-        verificaCartella( DIR_FILE . $cartellaFile );
+        verificaCartella(DIR_FILE . $cartellaFile);
     }
 
     # Percorso salvataggio pdf
@@ -180,19 +212,22 @@ try {
     # Percorso completo del file
     $file =  $dir . $nome_file;
 
-    if ( $isDEBUG ) {
+    if ($isDEBUG) {
         $file =  DIR_FILE . $nome_file;
     }
 
     # Genero pdf
-    $mpdf->Output( $file);
+    $mpdf->Output($file);
+
+
     $risposta->codiceErrore = errore::Success;
     $risposta->dati = $cartellaFile . $nome_file;
     $risposta->messaggio = $nome_file;
 
-    echo json_encode( $risposta );
+    $log->scrivi("PDF_energia", 3, "[Body] :", "FINE GENERAZIONE PDF");
 
-} catch ( Throwable $ex ) {
-    http_response_code( 500 );
-    $sendMail->emailErrore( 'Errore nella generazione PDF ABBONAMENTO', $ex->getMessage(), $ex->getFile(), $ex->getLine() );
+    echo json_encode($risposta);
+} catch (Throwable $ex) {
+    http_response_code(500);
+    $sendMail->emailErrore('Errore nella generazione PDF ENERGIA', $ex->getMessage(), $ex->getFile(), $ex->getLine());
 }
--- Core/function.php
+++ Core/function.php
@@ -1,298 +1,332 @@
 <?php
-function isEmpty($var)
-{
-    if ($var == "" || $var == null) {
+
+function isEmpty( $var ) {
+    if ( $var == '' || $var == null ) {
         return true;
     }
     return false;
 }
-function pulisci($value, $type, $db = '')
-{
+
+function pulisci( $value, $type, $db = '' ) {
     try {
 
-        switch ($type) {
+        switch ( $type ) {
 
             case 'string':
 
-                $res = filter_var($value, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-                break;
+            $res = filter_var( $value, FILTER_SANITIZE_FULL_SPECIAL_CHARS );
+            break;
 
             case 'email':
 
-                $res = filter_var($value, FILTER_SANITIZE_EMAIL);
-                break;
+            $res = filter_var( $value, FILTER_SANITIZE_EMAIL );
+            break;
 
             case 'number':
 
-                if (isset($value)) {
-                    $res = filter_var($value, FILTER_SANITIZE_NUMBER_INT);
-                } else {
+            if ( isset( $value ) ) {
+                $res = filter_var( $value, FILTER_SANITIZE_NUMBER_INT );
+            } else {
 
-                    $res = 'NULL';
-                }
+                $res = 'NULL';
+            }
 
-                break;
+            break;
             case 'bool':
 
-                $res = filter_var($value, FILTER_VALIDATE_BOOLEAN);
+            $res = filter_var( $value, FILTER_VALIDATE_BOOLEAN );
 
-                switch ($res) {
-                    case '':
-                    case false:
-                        $res = 0;
-                        break;
-
-                    default:
-                        $res = 1;
-                        break;
-                }
-
-
+            switch ( $res ) {
+                case '':
+                case false:
+                $res = 0;
                 break;
+
+                default:
+                $res = 1;
+                break;
+            }
+
+            break;
         }
 
-        // if (!isEmpty($db)) {
+        // if ( !isEmpty( $db ) ) {
 
-        //     $res = $db->real_escape_string(trim(strip_tags($res)));
+        //     $res = $db->real_escape_string( trim( strip_tags( $res ) ) );
         // }
 
         return $res;
-    } catch (Exception $ex) {
-        throw new Exception($ex->getMessage());
+    } catch ( Exception $ex ) {
+        throw new Exception( $ex->getMessage() );
     }
 }
 /**
- * Method dataAttuale
- *
- * @param $tipo $tipo [explicite description]
- *
- * @return void
- */
-function dataAttuale($tipo)
-{
-    switch ($tipo) {
+* Method dataAttuale
+*
+* @param $tipo $tipo [ explicite description ]
+*
+* @return void
+*/
+
+function dataAttuale( $tipo ) {
+    switch ( $tipo ) {
         case 'IT':
-            $d = date("d-m-Y"); //25-05-2020
-            break;
+        $d = date( 'd-m-Y' );
+        //25-05-2020
+        break;
         case 'IT-TIME':
-            $d = date("d/m/Y H:i"); //25-05-2020 11:41
-            break;
+        $d = date( 'd/m/Y H:i' );
+        //25-05-2020 11:41
+        break;
         case 'US':
-            $d = date("Y-m-d"); //2020-05-25
-            break;
+        $d = date( 'Y-m-d' );
+        //2020-05-25
+        break;
         case 'US-TIME':
-            $d = date("Y-m-d H:i:s"); //2020-05-25 11:41
-            break;
+        $d = date( 'Y-m-d H:i:s' );
+        //2020-05-25 11:41
+        break;
     }
     return $d;
 }
-function dataUsToIta($value)
-{
+
+function dataUsToIta( $value ) {
     try {
         // 2022-04-20 16:04:00 to 20/04/2022 16:04:00
 
-        $split = explode(" ", $value);
+        $split = explode( ' ', $value );
 
-        $data = explode("-", $split[0]);
+        $data = explode( '-', $split[ 0 ] );
 
-        return $data[2] . '/' . $data[1] . '/' . $data[0] . ' ' . substr($split[1], 0, 5);
-    } catch (Exception $ex) {
-        throw new Exception($ex->getMessage());
+        return $data[ 2 ] . '/' . $data[ 1 ] . '/' . $data[ 0 ] . ' ' . substr( $split[ 1 ], 0, 5 );
+    } catch ( Exception $ex ) {
+        throw new Exception( $ex->getMessage() );
     }
 }
-function apic($var)
-{
-    $varClear = str_replace("'", "''", $var);
+
+function apic( $var ) {
+    $varClear = str_replace( "'", "''", $var );
     return "'$var'";
 }
-function generateToken($length)
-{
+
+function generateToken( $length ) {
     $token = '';
     $possibleChars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $i = 0;
-    while ($i < $length) {
-        $char = substr($possibleChars, mt_rand(0, strlen($possibleChars) - 1), 1);
-        if (!strstr($token, $char)) {
+    while ( $i < $length ) {
+        $char = substr( $possibleChars, mt_rand( 0, strlen( $possibleChars ) - 1 ), 1 );
+        if ( !strstr( $token, $char ) ) {
             $token .= $char;
             $i++;
         }
     }
     return $token;
 }
-function generateOTP($length)
-{
+
+function generateOTP( $length ) {
     $token = '';
     $possibleChars = '0123456789';
     $i = 0;
-    while ($i < $length) {
-        $char = substr($possibleChars, mt_rand(0, strlen($possibleChars) - 1), 1);
-        if (!strstr($token, $char)) {
+    while ( $i < $length ) {
+        $char = substr( $possibleChars, mt_rand( 0, strlen( $possibleChars ) - 1 ), 1 );
+        if ( !strstr( $token, $char ) ) {
             $token .= $char;
             $i++;
         }
     }
     return $token;
 }
-function erroreTrovato($valore, $risposta)
-{
+
+function erroreTrovato( $valore, $risposta ) {
     $risposta->dati = '';
     $risposta->messaggio = $valore->getMessage();
     return $risposta;
 }
-function getToken()
-{
 
-    $token = bin2hex(random_bytes(16));
+function getToken() {
+
+    $token = bin2hex( random_bytes( 16 ) );
 
     return $token;
 }
-function getHeader($type)
-{
+
+function getHeader( $type ) {
     try {
-        foreach (getallheaders() as $name => $value) {
+        foreach ( getallheaders() as $name => $value ) {
 
-            if (strtolower($name) == strtolower($type)) {
+            if ( strtolower( $name ) == strtolower( $type ) ) {
 
-                return filter_var($value, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+                return filter_var( $value, FILTER_SANITIZE_FULL_SPECIAL_CHARS );
             }
         }
         return '';
-    } catch (Exception $ex) {
-        throw new Exception($ex->getMessage());
+    } catch ( Exception $ex ) {
+        throw new Exception( $ex->getMessage() );
     }
 }
-function somma_time()
-{
-    try {
-        // Somma time es. 01:00 + 00:30 = 01:30
-        $i = 0;
-        foreach (func_get_args() as $time) {
-            sscanf($time, '%d:%d', $hour, $min);
-            $i += $hour * 60 + $min;
-        }
 
-        if ($h = floor($i / 60)) {
-            $i %= 60;
-        }
+// function somma_time() {
+//     try {
+//         // Somma time es. 01:00 + 00:30 = 01:30
+//         $i = 0;
+//         foreach ( func_get_args() as $time ) {
+//             sscanf( $time, '%d:%d', $hour, $min );
+//             $i += $hour * 60 + $min;
+//         }
 
-        return sprintf('%02d:%02d', $h, $i);
-    } catch (Exception $ex) {
-        throw new Exception($ex->getMessage());
-    }
-}
-function oreINmin($value)
-{
+//         if ( $h = floor( $i / 60 ) ) {
+//             $i % = 60;
+//         }
+
+//         return sprintf( '%02d:%02d', $h, $i );
+//     } catch ( Exception $ex ) {
+//         throw new Exception( $ex->getMessage() );
+//     }
+// }
+
+function oreINmin( $value ) {
     // Conversione di ore in minuti es.  04:00:00 diventa 240 minuti
     try {
-        if (!isEmpty($value)) {
+        if ( !isEmpty( $value ) ) {
 
-            $split = explode(":", $value);
+            $split = explode( ':', $value );
 
-            $res = $split[0] * 60 + $split[1];
+            $res = $split[ 0 ] * 60 + $split[ 1 ];
 
             return $res;
         } else {
             return 0;
         }
-    } catch (Exception $ex) {
-        throw new Exception($ex->getMessage());
+    } catch ( Exception $ex ) {
+        throw new Exception( $ex->getMessage() );
     }
 }
-function check_ext($tipo)
-{
 
-    switch ($tipo) {
-        case "application/msword":
-            return true;
-            break;
-        case "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
-            return true;
-            break;
-        case "application/pdf":
-            return true;
-            break;
-        case "image/png":
-            return true;
-            break;
-        case "image/jpg":
-            return true;
-            break;
-        case "image/jpeg":
-            return true;
-            break;
-        case "image/gif":
-            return true;
-            break;
+function check_ext( $tipo ) {
+
+    switch ( $tipo ) {
+        case 'application/msword':
+        return true;
+        break;
+        case 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+        return true;
+        break;
+        case 'application/pdf':
+        return true;
+        break;
+        case 'image/png':
+        return true;
+        break;
+        case 'image/jpg':
+        return true;
+        break;
+        case 'image/jpeg':
+        return true;
+        break;
+        case 'image/gif':
+        return true;
+        break;
 
         default:
-            return false;
-            break;
+        return false;
+        break;
     }
 }
 // La funzione get_ext formatta l’estensione dell’immagine, infatti normalmente la variabile $_FILE
 // memorizza l’estensione come image/estensione, la funzione restituisce un risultato come .png.
-function get_ext($tipo)
-{
-    switch ($tipo) {
-        case "application/msword":
-            return "doc";
-            break;
-        case "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
-            return ".docx";
-            break;
-        case "application/pdf":
-            return ".pdf";
-            break;
-        case "image/png":
-            return ".png";
-            break;
-        case "image/jpg":
-            return ".jpg";
-            break;
-        case "image/jpeg":
-            return ".jpg";
-            break;
-        case "image/gif":
-            return ".gif";
-            break;
+
+function get_ext( $tipo ) {
+    switch ( $tipo ) {
+        case 'application/msword':
+        return 'doc';
+        break;
+        case 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+        return '.docx';
+        break;
+        case 'application/pdf':
+        return '.pdf';
+        break;
+        case 'image/png':
+        return '.png';
+        break;
+        case 'image/jpg':
+        return '.jpg';
+        break;
+        case 'image/jpeg':
+        return '.jpg';
+        break;
+        case 'image/gif':
+        return '.gif';
+        break;
 
         default:
-            return false;
-            break;
-    }
-}
-function verificaCartella($path)
-{
-
-    if (!file_exists($path)) {
-        mkdir($path, 0777);
+        return false;
+        break;
     }
 }
 
-function svuotaCartella($dirpath)
-{
-    $date = date('Y-m-d',strtotime("-3 days"));
+function verificaCartella( $path ) {
+
+    if ( !file_exists( $path ) ) {
+        mkdir( $path, 0777 );
+    }
+}
+
+function svuotaCartella( $dirpath ) {
+    $date = date( 'Y-m-d', strtotime( '-3 days' ) );
     try {
-        $handle = opendir($dirpath);
+        $handle = opendir( $dirpath );
 
-        while (($file = readdir($handle)) !== false) {
+        while ( ( $file = readdir( $handle ) ) !== false ) {
 
-            if ($file != '.' && $file != '..' && $file < $date) {
+            if ( $file != '.' && $file != '..' && $file < $date ) {
 
-                $files = glob($dirpath . $file . '/*'); //get all file names
-                foreach ($files as $filea) {
-                    if (is_file($filea))
-                        unlink($filea); //delete file
+                $files = glob( $dirpath . $file . '/*' );
+                //get all file names
+                foreach ( $files as $filea ) {
+                    if ( is_file( $filea ) )
+                    unlink( $filea );
+                    //delete file
                 }
 
-                rmdir($dirpath . $file);
+                rmdir( $dirpath . $file );
             }
         }
 
-        closedir($handle);
-    } catch (Throwable $ex) {
+        closedir( $handle );
+    } catch ( Throwable $ex ) {
         echo $ex->getMessage();
     }
 }
 
+function dataAttualePDF() {
 
+    date_default_timezone_set( 'Europe/Rome' );
+
+    // Ottieni la data corrente formattata
+    $formattedDate = date( 'M d, Y' );
+    // Ad esempio, 'Oct 23, 2023'
+
+    // Effettua la conversione del mese in italiano
+    $italianMonths = [
+        'Jan' => 'GEN',
+        'Feb' => 'FEB',
+        'Mar' => 'MAR',
+        'Apr' => 'APR',
+        'May' => 'MAG',
+        'Jun' => 'GIU',
+        'Jul' => 'LUG',
+        'Aug' => 'AGO',
+        'Sep' => 'SET',
+        'Oct' => 'OTT',
+        'Nov' => 'NOV',
+        'Dec' => 'DIC'
+    ];
+
+    $formattedDate = strtr( $formattedDate, $italianMonths );
+
+
+    return $formattedDate;
+
+
+}
